de296745083776b75571357531c875800ef813f5
ELY909 <145435556+ELY909@users.noreply.github.com>
2024-03-08 19:57:49 +0700

    POSSTEST1_PBO

---
+++ POSSTEST1/Baju.java
@@ -0,0 +1,11 @@
+public class Baju {
+    String kode;
+    String nama;
+    int harga;
+
+    public Baju(String kode, String nama, int harga) {
+        this.kode = kode;
+        this.nama = nama;
+        this.harga = harga;
+    }
+}
