7482603475ca536507e6ab1d7d8f8d2e94af7908
ELY909 <145435556+ELY909@users.noreply.github.com>
2024-03-17 13:15:58 +0700

    Add files via upload
    Nim : 2209106009
    Nama : Muhammad Arif Rachman

---
+++ POSSTEST2/POSSTEST2/src/Toko/Baju.java
@@ -0,0 +1,37 @@
+package Toko;
+
+public class Baju {
+    private String kode;
+    private String nama;
+    private int harga;
+
+    public Baju(String kode, String nama, int harga) {
+        this.kode = kode;
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
